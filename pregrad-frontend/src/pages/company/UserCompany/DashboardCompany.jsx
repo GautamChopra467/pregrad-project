51af2356029dc00c37f3883291b8bb022c8edd36
GautamChopra467 <harshchopra467@gmail.com>
2022-08-09 02:28:30 -0700

    Twenty First commit by Gautam Chopra

--- pregrad-frontend/src/pages/company/UserCompany/DashboardCompany.jsx
+++ pregrad-frontend/src/pages/company/UserCompany/DashboardCompany.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSettings } from 'react-icons/fi';
 import { HiOutlinePencil } from "react-icons/hi";
 import { MdOutlinePeopleAlt } from 'react-icons/md';
@@ -7,6 +7,86 @@ import "../../../components/company/css/UserCompany/DashboardCompanyStyles.css";
 
 const DashboardCompany = () => {
   const navigate = useNavigate();
+
+  // EDIT FORM
+  const [isModal, setIsModal] = useState(false);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const locationData = ["Delhi, New Delhi", "Mumbai", "Chennai", "Jaipur", "Hyderabad"]
+  const [selectedLocation, setSelectedLocation] = useState("");
+ 
+  const handleLocation = (event) => {
+    setSelectedLocation(event.target.value);
+  }
+
+  const [companyInfo, setCompanyInfo] = useState({
+    name: "",
+    companyname: "",
+    designation: "",
+    websitelink: "",
+    mobile: ""
+  });
+
+  const handleForm = (e) => {
+    const {name, value} = e.target;
+    setCompanyInfo({
+      ...companyInfo,
+      [name]: value
+    })
+   
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(companyInfo));
+    setIsSubmit(true);
+   
+  }
+
+  useEffect(() => {
+    if( Object.keys(formErrors).length === 0 && isSubmit ){
+      console.log("submitted")
+    }
+  },[formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+
+    if(!values.name){
+      errors.name = "Name Required"
+    }else if(values.name.length < 3){
+      errors.name = "Minimum 3 characters required"
+    }else if(values.name.length > 18){
+      errors.name = "Maximum 18 characters required";
+    }
+
+    if(!values.companyname){
+      errors.companyname = "Company name required"
+    }else if(values.companyname.length < 2){
+      errors.companyname = "Minimum 2 characters required"
+    }else if(values.companyname.length > 18){
+      errors.companyname = "Maximum 18 characters required";
+    }
+    
+    if(!values.designation){
+      errors.designation = "Designation required"
+    }else if(values.designation.length > 20){
+      errors.designation = "Maximum 20 characters required";
+    }
+
+    if(!values.mobile){
+      errors.mobile = "Mobile number required"
+    }else if(values.mobile.length !== 10){
+      errors.mobile = "Mobile number is Invalid";
+    }
+
+    if(!selectedLocation){
+      errors.location = "Location required"
+    }
+
+    return errors;
+  }
 
   return (
     <div>
@@ -37,10 +117,10 @@ const DashboardCompany = () => {
                   <FiSettings className='settings_icon_dashboardCompany' />
                   <h2>Account Settings</h2>
                 </div>
-                <Link to="/">Change</Link>
+                <p onClick={() => setIsModal(!isModal)}>Change</p>
               </div>
               <div className='bottom_box_account_dashboardCompany'>
-                <p>Change your name, company type, etc. from here.</p>
+                <p>Change your name, location, company type, etc. from here.</p>
               </div>
             </div>
 
@@ -48,12 +128,12 @@ const DashboardCompany = () => {
               <div className='top_box_applicants_dashboardCompany'>
                 <div>
                   <MdOutlinePeopleAlt className='people_icon_dashboardCompany' />
-                  <h2>New Applicants&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
+                  <h2>New Applicants&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h2>
                 </div>
                 <Link to="/">View</Link>
               </div>
               <div className='bottom_box_applicants_dashboardCompany'>
-                <p>Check new applicants applied for your internship post.</p>
+                <p>Check new applicants applied for your internship post.&nbsp;&nbsp;&nbsp;&nbsp;</p>
               </div>
             </div>
           </div>
@@ -66,6 +146,70 @@ const DashboardCompany = () => {
           </div>
         </div>
       </div>
+
+
+      {isModal && (
+        
+        <div className='modal_backgound_dashboardCompany'>
+        <div className='modal_container_dashboardCompany'>
+          <div className='modal_top_section_dashboardCompany'>
+            <h2>Edit Account Details</h2>
+            <p className="errors_msg_dashboardCompany">{formErrors.others}</p>
+          </div>
+ 
+          <div className='modal_mid_section_dashboardCompany'>
+            <form>
+              <div className="form_box_dashboardCompany">
+                <label>Name</label>
+                <input type="text" name="name" placeholder="Enter Your Name" onChange={handleForm} />
+                <p className="errors_msg_dashboardCompany">{formErrors.name}</p>
+              </div>
+ 
+              <div className="form_box_dashboardCompany">
+                <label>Company Name</label>
+                <input type="text" name="companyname" placeholder="Enter Company Name" onChange={handleForm} />
+                  <p className="errors_msg_dashboardCompany">{formErrors.companyname}</p>
+              </div>
+ 
+              <div className="form_box_dashboardCompany">
+                <label>Designation</label>
+                <input type="text" name="designation" placeholder="Enter Your Designation" onChange={handleForm} />
+                <p className="errors_msg_dashboardCompany">{formErrors.designation}</p>
+              </div>
+
+              <div className="form_box_dashboardCompany">
+                   <label>Location of Headquarters</label>
+                   <select onChange={handleLocation} className="select_dashboardCompany">
+                    <option value="">Enter Location</option> 
+                    {locationData.map(val => (
+                      <option key={val} value={val}>{val}</option>
+                    ))}
+                    </select>
+                   <p className="errors_msg_dashboardCompany">{formErrors.location}</p>
+                 </div>
+ 
+                 <div className="form_box_dashboardCompany">
+                   <label>Company Website Link</label>
+                   <input type="url" name="websitelink" placeholder="Enter Website Link" onChange={handleForm} />
+                   <p className="errors_msg_dashboardCompany">{formErrors.websitelink}</p>
+                 </div>
+
+                 <div className="form_box_dashboardCompany">
+                   <label>Mobile Number</label>
+                   <input type="number" name="mobile" placeholder="Enter Phone Number" onChange={handleForm} />
+                   <p className="errors_msg_dashboardCompany">{formErrors.mobile}</p>
+                 </div>
+ 
+                 <div className='modal_bottom_section_dashboardCompany'>
+                   <button onClick={() => setIsModal(!isModal)} className='btn_light_dashboardCompany'>Cancel</button>
+                   <button type='submit' onClick={submitForm} className='btn_primary_dashboardCompany'>Save Details</button>
+                 </div>
+               </form>
+          </div>
+         </div>
+      </div>
+       
+      )}
     </div>
   )
 }
--- pregrad-frontend/src/pages/company/UserCompany/ProfileCompany.jsx
+++ pregrad-frontend/src/pages/company/UserCompany/ProfileCompany.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from 'react';
+import "../../../components/company/css/UserCompany/ProfileCompanyStyles.css";
+import ProfileBackground from "../../../img/profile-background.jpg";
+import { BsLinkedin } from "react-icons/bs";
 
 const ProfileCompany = () => {
   return (
     <div>
-      
+      <div className="main_container_profilecompany">
+        <div className="welcome_container_profilecompany">
+          <div className="welcome_left_section_profilecompany">
+            <h4>Hi, welcome back!</h4>
+            <p>Your Profile</p>
+          </div>
+          <div className="welcome_right_section_profilecompany">
+            <h4>
+              App <span>/ Profile</span>
+            </h4>
+          </div>
+        </div>
+
+        <div className="profile_container_profilecompany">
+          <div className="profile_background_profilecompany">
+            <img src={ProfileBackground} alt="background" />
+            <div className="profile_edit2_container_profilecompany">
+              <div className="profile_edit2_profilecompany">
+                  <BsLinkedin size={18} />
+              </div>
+            </div>
+          </div>
+
+          <div className="profile_user_details_profilecompany">
+            <div className="user_image_profilecompany">
+              G
+            </div>
+            <div className="profile_info_profilecompany">
+              <div className="info_container_profilecompany">
+                <div className="info_left_section_profilecompany">
+                  <h5>Google</h5>
+                  <p>Private Organisation</p>
+                </div>
+                <div className="info_middle_section_profilecompany">
+                  <h5>harshchopra467@gmail.com</h5>
+                </div>
+              </div>
+
+              <div className="profile_edit_container_profilecompany">
+                <div className="profile_edit_profilecompany">
+                  <BsLinkedin />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='main_details_container_profilecompany'>
+          <div className='main_details_left_section_profilecompany'>
+            <div className='card_profilecompany'>
+              <h4>Representative</h4>
+              <div className="line_profilecompany"></div>
+              <div className='owner_details_container'>
+                <h3>Gautam Chopra</h3>
+                <p>H.R.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className='main_details_right_section_profilecompany'>
+            <div className='card_profilecompany'>
+            <h4>About Company</h4>
+              <div className="line_profilecompany"></div>
+              <div className='owner_details_container'>
+                <h3>Location | Established In</h3>
+                <p>Delhi, India | From 2002</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='card_profilecompany'>
+              <h4>Description of Company</h4>
+              <div className="line_profilecompany"></div>
+              <div className='owner_details_container'>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+              </div>
+            </div>
+      </div>
     </div>
   )
 }
