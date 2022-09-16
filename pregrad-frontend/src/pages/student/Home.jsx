de9e9a8eefd042a198db05b442257d0f9bd5d09e
GautamChopra467 <harshchopra467@gmail.com>
2022-09-15 22:09:35 -0700

    Fifty Third Commit by Gautam Chopra

--- pregrad-frontend/src/pages/student/Home.jsx
+++ pregrad-frontend/src/pages/student/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import Footer from '../../components/student/jsx/Footer';
 import Header from '../../components/student/jsx/Header';
 import HomeBanner1 from '../../components/student/jsx/HomeBanner1';
+import HomeBanner2 from '../../components/student/jsx/HomeBanner2';
+import HomeBanner3 from '../../components/student/jsx/HomeBanner3';
+import SliderCompanies from '../../components/student/jsx/SliderCompanies';
 
 const Home = ({theme, setTheme}) => {
   return (
     <div>
       <Header theme={theme} setTheme={setTheme} />
       <HomeBanner1 />
+      <SliderCompanies />
+      <HomeBanner3 />
+      <HomeBanner2 />
+      <Footer />
     </div>
   )
 }
 
-export default Home
+export default Home;
